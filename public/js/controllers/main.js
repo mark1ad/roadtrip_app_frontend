5abc655f7a046419eb75ac2a12162e79e498d902
dnialwill <dnialwill@gmail.com>
2017-05-17 18:47:15 -0400

    Made angular app module and main controller

---
+++ public/js/controllers/main.js
@@ -0,0 +1,50 @@
+(function() {
+
+  /* Creates Angular controller 'MainController' */
+  angular
+    .module('roadtripApp')
+    .controller('MainController', MainController);
+
+  /* Dependency injection */
+  MainController.$inject = ['$http'];
+
+  /* Controller constructor function */
+  function MainController($http) {
+
+    /* Helper variables */
+    const URL = 'http://localhost:3000/';
+    const vm = this;
+
+    /* Controller properties */
+    vm.getUsers = getUsers;
+
+    /* Initialization */
+    activate();
+
+    // Function declarations //
+
+    /* Initialization function */
+    function activate() {
+      vm.getUsers();
+    }
+
+    /* Gets user data from Rails server */
+    function getUsers() {
+      $http(request('users'))
+      .then(function(response) {
+        console.log(response);
+      });
+    }
+
+    /* Creates $http request object */
+    function request(route = '', method = 'GET', data = null) {
+      return {
+        method: method,
+        url: URL + route,
+        data: data
+      };
+    }
+
+  }
+
+})();
