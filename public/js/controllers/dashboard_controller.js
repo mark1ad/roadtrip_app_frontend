1258ba9c0ceae42e19f84f95517f8f5f41aa486f
Ellen <ellendoornbos39@gmail.com>
2017-05-19 19:31:52 -0400

    Frontend updates with creation and deletion actions

--- public/js/controllers/dashboard_controller.js
+++ public/js/controllers/dashboard_controller.js
@@ -24,6 +24,7 @@
     /* Controller properties */
     vm.createNewRoadtrip = createNewRoadtrip;
     vm.currentUserId = '1'; // <<< hard coded, in development
+    vm.deleteRoadtrip = deleteRoadtrip;
     vm.getUserRoadtrips = getUserRoadtrips;
     vm.currentUserRoadtrips = [];
     vm.newRoadtrip = {};
@@ -56,11 +57,21 @@
     function createNewRoadtrip() {
       var newRoadtripId;
       $http(request('users/' + vm.currentUserId + '/roadtrips', 'POST', vm.newRoadtrip))
-      .then(function(response) {
-        vm.newRoadtrip = {};
-        console.log(response);
+        .then(function(response) {
+          vm.currentUserRoadtrips.push(response.data)
+          vm.newRoadtrip = {};
+          console.log(response);
       });
     };
+
+    /* Delete a Roadtrip */
+    function deleteRoadtrip(id, index) {
+      $http(request('users/' + vm.currentUserId + '/roadtrips/' + id, 'DELETE'))
+       .then(function(response) {
+         vm.currentUserRoadtrips.splice(index, 1);
+         console.log(response.data);
+       })
+    }
 
   }
 
