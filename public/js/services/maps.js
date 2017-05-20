96dada35552f9b3030c8c797ff6cb7ca1732a112
dnialwill <dnialwill@gmail.com>
2017-05-20 14:30:51 -0400

    More map service functions

--- public/js/services/maps.js
+++ public/js/services/maps.js
@@ -17,27 +17,52 @@
     /* Maps service definition */
     var service = {
       addMarker: addMarker,
+      clearMarkers: clearMarkers,
       convertLatLngObj: convertLatLngObj,
+      deleteMarkers: deleteMarkers,
       drawMap: drawMap,
       getCoordinates: getCoordinates,
-      markers: markers
+      markers: markers,
+      setMapOnAll: setMapOnAll,
+      showMarkers: showMarkers
     }
     return service;
 
     // Function Declarations //
-    /* A lot of these are derived from examples in the documentation */
+    /* A lot of these are derived or taken from examples in the documentation */
 
     /* Adds marker to map based on given lat/long object */
     function addMarker(latLngObj) {
-      var marker = new google.maps.Marker();
+      var marker = new google.maps.Marker({ animation: google.maps.Animation.DROP });
       marker.setPosition(latLngObj);
       marker.setMap(map);
       markers.push(marker);
     }
 
+    /* Hides markers on map */
+    function clearMarkers() {
+      setMapOnAll(null)
+    }
+
     /* Converts given latitude and longitude to obj for maps use */
     function convertLatLngObj(lat, lng) {
       return { lat: lat, lng: lng };
+    }
+
+    /* Hides markers and removes them from the markers array */
+    function deleteMarkers() {
+      clearMarkers();
+      markers = [];
+    }
+
+    /* Draws map on div#map centered on given coordinates obj */
+    function drawMap(centerCoordinatesObj) {
+      let options = {
+        zoom: 8,
+        center: centerCoordinatesObj,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('map'), options);
     }
 
     /* Geocodes coordinates based on search string */
@@ -52,14 +77,16 @@
       });
     }
 
-    /* Draws map on div#map centered on given coordinates obj */
-    function drawMap(centerCoordinatesObj) {
-      let options = {
-        zoom: 8,
-        center: centerCoordinatesObj,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      map = new google.maps.Map(document.getElementById('map'), options);
+    /* Can be used to change visibility status of markers */
+    function setMapOnAll(map) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+    }
+
+    /* Makes all markers visible */
+    function showMarkers() {
+      setMapOnAll(map);
     }
 
   }
